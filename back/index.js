cd126f0c8fb6d7d9ec588fc36520f9c7cc68eb8a
Ragsomar <demarspierrealban@gmail.com>
2020-04-17 16:18:10 +0200

    Add back files

---
+++ back/index.js
@@ -0,0 +1,42 @@
+const express = require('express')
+const app = express()
+const port = 3000
+const connection = require('./config')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(cors())
+
+app.get('/', (request, response) => {
+	response.send('Welcome to Grifo Auto server')
+})
+
+app.get('/', (request, response) => {
+	connection.query('SELECT * FROM ', (err, results) => {
+		if (err) {
+			console.log(err)
+			response.status(500).send('Erreur retrieving ')
+		} else {
+			response.json(results)
+		}
+	})
+})
+app.get('/', (request, response) => {
+	connection.query('SELECT * FROM ', (err, results) => {
+		if (err) {
+			console.log(err)
+			response.status(500).send('Erreur retrieving ')
+		} else {
+			response.json(results)
+		}
+	})
+})
+
+app.listen(port, (err) => {
+	if (err) {
+		throw new Error('Something bad happened ...')
+	}
+	console.log(`Server is listening on ${port}`)
+})

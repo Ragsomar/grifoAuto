e37150ff2ff3886618610ef5a71d9c738babc056
Ragsomar <demarspierrealban@gmail.com>
2020-04-17 18:03:13 +0200

    Get modele name in App and boolean for loading

--- front/src/App.js
+++ front/src/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Homepage from './components/Homepage'
 import Header from './components/Header'
 import ProductPage from './components/ProductPage'
+import axios from 'axios'
 import './style/layout.css'
 import './style/space.css'
 import './style/separation.css'
@@ -12,11 +13,25 @@ import './style/image.css'
 class App extends React.Component {
 	constructor(props) {
 		super(props)
-		this.state = {}
+		this.state = {
+			name: null,
+			isLoaded: false,
+		}
+	}
+
+	componentDidMount() {
+		const url = 'http://localhost:3000/home'
+		axios
+			.get(url)
+			.then((data) =>
+				this.setState({ name: data.data[0].modele, isLoaded: true })
+			)
 	}
 
 	render() {
-		return (
+		return this.state.isLoaded !== true ? (
+			<h2>Loading ...</h2>
+		) : (
 			<div className='height-max-100 flex-column' id='outer-container'>
 				<Header />
 				<main className='flex1 overflow height-max-100' id='page-wrap'>

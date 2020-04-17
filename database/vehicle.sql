58d5c200878c6a0159f907b87afe16117320af96
Ragsomar <demarspierrealban@gmail.com>
2020-04-17 16:20:07 +0200

    Add database scripts

---
+++ database/vehicle.sql
@@ -0,0 +1,12 @@
+USE grifoauto;
+
+INSERT INTO
+    vehicle (id, modele, years, motor, interior)
+VALUES
+    (
+        1,
+        "FIAT 500 F",
+        "1965 - 1972",
+        "199cc",
+        "Simili"
+    )

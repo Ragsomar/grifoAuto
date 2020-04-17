fe893a0169214874bf7fb94d17f1015dee1c20f6
Ragsomar <demarspierrealban@gmail.com>
2020-04-17 17:52:18 +0200

    Delete useless tables atm

--- database/database.sql
+++ database/database.sql
@@ -7,19 +7,7 @@ USE grifoauto;
 CREATE TABLE vehicle (
     id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
     modele VARCHAR (100) NOT NULL,
-    years VARCHAR (10) NOT NULL,
-    motor VARCHAR (50) NOT NULL,
-    interior VARCHAR (25) NULL
-),
-CREATE TABLE photo (
-    id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
-    photo,
-    modele VARCHAR (100) NOT NULL,
-    color VARCHAR (100) NOT NULL
-),
-CREATE TABLE evalable vehicle (
-    id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
-    years years VARCHAR (10) NOT NULL,
+    years VARCHAR (11) NOT NULL,
     motor VARCHAR (50) NOT NULL,
     interior VARCHAR (25) NULL
 );

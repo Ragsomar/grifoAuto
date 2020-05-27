f52121f51ba783b759cea258e95d4c84339e548c
Ragsomar <demarspierrealban@gmail.com>
2020-05-28 01:23:05 +0200

    ADD Axios and get datas

--- front/src/App.js
+++ front/src/App.js
@@ -14,14 +14,14 @@ class App extends React.Component {
 	constructor(props) {
 		super(props)
 		this.state = {
-			modele: '',
+			photo: [],
 		}
 	}
 
 	componentDidMount() {
-		const url = 'https://localhost:3000/home'
+		const url = 'http://localhost:3000/home'
 		axios.get(url).then((res) => {
-			this.setState({ photo: res.data[0].modele })
+			this.setState({ photo: res.data })
 		})
 	}
 
@@ -30,7 +30,7 @@ class App extends React.Component {
 			<div className='height-max-100 flex-column' id='outer-container'>
 				<Header />
 				<main className='flex1 overflow height-max-100' id='page-wrap'>
-					<Homepage />
+					<Homepage photos={this.state.photo} />
 					<ProductPage />
 				</main>
 			</div>
